87ce5df11d580b17a0db05fd68ca1c3c72ddc439
senin26 <sergey.ionin.7@gmail.com>
2019-07-02 11:30:32 +0300

    just a commit from a CLI

--- Senin26Cloud/src/main/java/basic_client/EasyClient.java
+++ Senin26Cloud/src/main/java/basic_client/EasyClient.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class EasyClient {
     public static void main(String[] args) {
         Socket socket;
-        String fileName = "/ServerTXTdoc.txt";
+        String fileName = "/ServerTXTdoc.txt"; //this is a filename
         String filePathSrc = "/home/serj/Java/GeekBrains/GEEKBRAINS/JavaCloudStorage/Senin26Cloud/src/main/java/basic_server/serverstorage";
         String filePathDest = "/home/serj/Java/GeekBrains/GEEKBRAINS/JavaCloudStorage/Senin26Cloud/src/main/java/basic_client/clientstorage";
         Thread clientInThread = new Thread(() -> {
